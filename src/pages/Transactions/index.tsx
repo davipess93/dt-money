fb880db33053ccb737b34483887b494375fc47e8
Davi Pessoa <davialvespessoa@gmail.com>
2023-05-02 12:14:33 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="45%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>02/05/2023</td>
+            </tr>
+            <tr>
+              <td width="45%">Humburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>02/05/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
